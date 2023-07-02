e159462c8946eaceab5bba0d84712cc93a3cf287
Rishabh Jain <jainrishabh.201@gmail.com>
2023-07-02 23:53:22 +0530

    Initial Setup

---
+++ SystemEngine/main.py
@@ -0,0 +1,23 @@
+#System Engine (Main Function)
+#Welcome DashBoard
+
+
+#Workflow : 
+#Event & Host Details
+#Ticket Booking & Seat Configuration
+#Security Checks (AI Model and Face Recognition System)
+#Event Attendance (Using Face Recognition)
+#Event Feedback (Auto Audio Sanmpling)
+#Event Reminder Alert (Auto Emailing)
+#Event Summary Report  (Total Ticket Sold , Profit Earned etc.)
+#Invoice Mailing  (Auto Mailing System)
+        
+#importingmodules
+
+
+#MainFunctiion
+
+print("Hello World!")
+print("Welcome to BEMSystem \nDeveloped by Rishabh Jain & Utkarsh Jha")
+
+
