8342a373d619c1c5deb393e84f6d98e5731cb52f
Utkarsh Jha <utkarshjhaonline@gmail.com>
2023-07-08 00:24:58 +0530

    event page with required improvements

--- SystemEngine/events and bookings/event.py
+++ SystemEngine/events and bookings/event.py
@@ -2,6 +2,7 @@
 
 import pymongo
 import time
+from datetime import datetime
 import random
 import string
 # conneting to mongoDB 
@@ -14,37 +15,79 @@ collection = mongo["users"]
 def event_enquiry():
     print("Event enquiry will be created")
 
-# for event we will take all data as input and store it in dictonary
 
 def event_details():
     print("Event registration starting")
     print("Enter all the details carefully")
     time.sleep(3)
-    event_id = input("Enter a unique event ID for future Refference")
-    event_name = input("Enter event name:  ")
-    event_venue = input("Enter event venue:  ")
-    event_date = input("Enter event date:  ")
-    event_type = input("Enter event type: ")
-    event_attendies = input("Enter the no. of expected attendies:  ")
-    event_organizers = input("Enter no. of event organizers:  ")
-    event_host_contact = input("Enter event ")
+
+#                                                                                      Event registration
+
+    event_ID = int(input("Enter a unique event ID for future Refference"))
+    event_Name = input("Enter event name:  ")
+    event_Venue = input("Enter event venue:  ")
+    date_string = input("Enter a date (YYYY-MM-DD): ")
+    date = datetime.strptime(date_string, "%Y-%m-%d")
+    # event_Date = input("Enter event date:  ")
+    event_Type = input("Enter event type: ")
+    event_Attendies = input("Enter the no. of expected attendies:  ")
+    event_Organizers = input("Enter no. of event organizers:  ")
+    event_Host_Contact = input("Enter event ")
     host_name = input("Enter the name of the Host: ")
 
+
+#                                                                                       Ticketing process 
+
+
+    print("Ticketing: \n")
+    time.sleep(1)
+    ticket_type = input("Does your ticketing scheme follow any scheme, Enter Y if yes, enter N if no")
+
+    ticket_Price_Single = 0
+    ticket_Quantity = 0 
+    
+    if ticket_type.upper() == 'Y':
+        num  = int(input("Enter the no of ticket schemes you are offering"))
+        ticket_data = { }
+        list_of_Prices = []
+        for e in range(num):
+            key = input("Enter the Booking type (Premimum, Gold, Platinum etc.)")
+            value = int(input("Enter the no. of seats in "+key ": "))
+            price = float(input("Enter the cost of each " +key " class ticket: "))
+            ticket_data[key] = value
+            list_of_Prices.append(price)
+
+
+    elif ticket_type.upper() == 'N':
+        ticket_Quantity = int(input("Enter the no. of tickets you want to sell: "))
+        ticket_Price_Single = float(input("Enter price of single ticket: "))
+
+
+
+
+
+
+
     event_data = {
-            "event_id": event_id,
+            "event_ID": event_ID,
             "host_name": host_name,
-            "event_name": event_name,
-            "event_venue": event_venue,
-            "event_date": event_date,
-            "event_type": event_type,
-            "event_attendies": event_attendies,
-            "event_organizers": event_organizers,
-            "event_host_contact": event_host_contact
+            "event_Name": event_Name,
+            "event_Venue": event_Venue,
+            "event_Date": date,
+            "event_Type": event_Type,
+            "event_Attendies": event_Attendies,
+            "event_Organizers": event_Organizers,
+            "event_Host_Contact": event_Host_Contact,
+            "ticket_Quantity": ticket_Quantity,
+            "ticket_Price_Single": ticket_Price_Single,
+            "tickets_Schemas" : ticket_data,
+            "tickets_Schemas_priceList" : list_of_Prices
+
     }
     collection.insert_many(event_data)
     time.sleep(1)
     print("Event registration complete")
-    print(event_name + " Has been scheduled for " + event_date)
+    print(event_Name + " Has been scheduled for " + date)
 
 event_details()
 myclient.close()
