49ff9ef1e14a218591c478e2cb38607fbfeed99f
Rishabh Jain <jainrishabh.201@gmail.com>
2023-07-09 16:48:11 +0530

    accounts created

--- SystemEngine/eventsAndBookings/accounts.py
+++ SystemEngine/eventsAndBookings/accounts.py
@@ -1,1 +1,17 @@
-#Balance Shets
+#                                                       by RishabhJain2010          
+
+def totalsale(newsale):
+    
+    totalsale=+newsale
+    return totalsale
+
+def taxdeduction(totalsale):
+    taxdeduction= totalsale*0.18
+    return taxdeduction 
+
+def netprofit(totalsale,taxdeduction):
+    netprofit=totalsale-taxdeduction
+    return netprofit
+
+
+    
