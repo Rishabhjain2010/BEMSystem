08248be8efe1b0ad50b0762778dc36c4a59e4f8c
Rishabh Jain <jainrishabh.201@gmail.com>
2023-07-12 05:04:29 +0530

    sale records created!

--- SystemEngine/eventsAndBookings/records.py
+++ SystemEngine/eventsAndBookings/records.py
@@ -1,1 +1,27 @@
-# members records
+#                                                             By Rishabhajain2010   
+
+#This fucntion shows all tickets sold for a particular event ID entered by user in recent sale last format!
+
+import dbconnect from
+from tabulate import tabulate
+
+def show_eventsale() :
+
+    #FETCH EVENT ID
+    event_id = int(input("Enter Event ID: "))
+    print("Please wait while we fetch the details! ")
+    time.sleep(5)
+    
+    #Fetch Event Deatils 
+    sale=collection.find({"event_id":event_id})        
+    
+    sale_list = list(sale)
+    if sale_list:
+        field_names = sale_list[0].keys()
+        table = tabulate(sale_list, headers=field_names, tablefmt="pretty")
+        print(table)
+    else:
+        print("No sales found for the specified Event ID.")
+        print("Please Try Again!")
+        show_eventsale()
+
